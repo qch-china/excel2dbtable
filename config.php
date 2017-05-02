58ef54cf4a962d6a191c795a79cc7f39be778d5a
quanchaohao <aa1080711@163.com>
2017-05-02 14:00:48 +0800

    调整测试文件和config文件的格式

--- config.php
+++ config.php
@@ -1,13 +1,30 @@
 <?php
 
 return array(
-    'db_host' => 'localhost',       //数据库host
-    'db_user' => 'root',            //数据库用户名
-    'db_pwd' => '',                 //数据库密码
-    'db_name' => 'test',            //数据库名
-    'table_name' => 'test',         //表名
-    'excel_name' => 'test_file/test.xlsx',    //excel名称（放在目录excel_file下）
-    'drop_table' => true,           //插入之前，是否先删表
-    'show_detail' => false,         //是否显示详细信息
-    'field_length' => 1024,         //默认的表字段varchar长度
+    //数据库host
+    'db_host'      => 'localhost',
+
+    //数据库用户名
+    'db_user'      => 'root',
+
+    //数据库密码
+    'db_pwd'       => '',
+
+    //数据库名
+    'db_name'      => 'test',
+
+    //表名
+    'table_name'   => 'test',
+
+    //excel名称
+    'excel_name'   => 'test_file/test.xlsx',
+
+    //插入之前，是否先删表
+    'drop_table'   => true,
+
+    //是否显示详细信息
+    'show_detail'  => false,
+
+    //默认的表字段varchar长度
+    'field_length' => 1024,
 );
